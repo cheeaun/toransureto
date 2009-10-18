4bd3890e66e9caa09b2c5cfd4f2814ac5ba419a5
Lim Chee Aun <cheeaun@gmail.com>
2009-10-19 01:47:29 +0800

    - Removed languages.dat - Changed the jap-romaji translator to Tatoeba Project

--- main.py
+++ main.py
@@ -4,25 +4,72 @@ import os
 import wsgiref.handlers
 from cgi import escape
 from re import compile
-from urllib import urlencode, quote
-from xml.dom.minidom import parseString
+from urllib import urlencode
 from django.utils import simplejson
 from google.appengine.api import urlfetch
 from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+
+LANGUAGES = [
+  {'code': '', 'lang': 'Detect language'},
+  {'code': 'af', 'lang': 'Afrikaans'},
+  {'code': 'sq', 'lang': 'Albanian'},
+  {'code': 'ar', 'lang': 'Arabic'},
+  {'code': 'be', 'lang': 'Belarusian'},
+  {'code': 'bg', 'lang': 'Bulgarian'},
+  {'code': 'ca', 'lang': 'Catalan'},
+  {'code': 'zh-CN', 'lang': 'Chinese'},
+  {'code': 'hr', 'lang': 'Croatian'},
+  {'code': 'cs', 'lang': 'Czech'},
+  {'code': 'da', 'lang': 'Danish'},
+  {'code': 'nl', 'lang': 'Dutch'},
+  {'code': 'en', 'lang': 'English'},
+  {'code': 'et', 'lang': 'Estonian'},
+  {'code': 'tl', 'lang': 'Filipino'},
+  {'code': 'fi', 'lang': 'Finnish'},
+  {'code': 'fr', 'lang': 'French'},
+  {'code': 'gl', 'lang': 'Galician'},
+  {'code': 'de', 'lang': 'German'},
+  {'code': 'el', 'lang': 'Greek'},
+  {'code': 'iw', 'lang': 'Hebrew'},
+  {'code': 'hi', 'lang': 'Hindi'},
+  {'code': 'hu', 'lang': 'Hungarian'},
+  {'code': 'is', 'lang': 'Icelandic'},
+  {'code': 'id', 'lang': 'Indonesian'},
+  {'code': 'ga', 'lang': 'Irish'},
+  {'code': 'it', 'lang': 'Italian'},
+  {'code': 'ja', 'lang': 'Japanese'},
+  {'code': 'ko', 'lang': 'Korean'},
+  {'code': 'lv', 'lang': 'Latvian'},
+  {'code': 'lt', 'lang': 'Lithuanian'},
+  {'code': 'mk', 'lang': 'Macedonian'},
+  {'code': 'ms', 'lang': 'Malay'},
+  {'code': 'mt', 'lang': 'Maltese'},
+  {'code': 'no', 'lang': 'Norwegian'},
+  {'code': 'fa', 'lang': 'Persian'},
+  {'code': 'pl', 'lang': 'Polish'},
+  {'code': 'pt', 'lang': 'Portuguese'},
+  {'code': 'ro', 'lang': 'Romanian'},
+  {'code': 'ru', 'lang': 'Russian'},
+  {'code': 'sr', 'lang': 'Serbian'},
+  {'code': 'sk', 'lang': 'Slovak'},
+  {'code': 'sl', 'lang': 'Slovenian'},
+  {'code': 'es', 'lang': 'Spanish'},
+  {'code': 'sw', 'lang': 'Swahili'},
+  {'code': 'sv', 'lang': 'Swedish'},
+  {'code': 'th', 'lang': 'Thai'},
+  {'code': 'tr', 'lang': 'Turkish'},
+  {'code': 'uk', 'lang': 'Ukrainian'},
+  {'code': 'vi', 'lang': 'Vietnamese'},
+  {'code': 'cy', 'lang': 'Welsh'},
+  {'code': 'yi', 'lang': 'Yiddish'}
+]
 
 class MainHandler(webapp.RequestHandler):
   def get(self):
-    languages = memcache.get('languages')
-    if languages is None:
-      file = 'languages.dat'
-      if os.path.isfile(file):
-        content = open(file, 'r')
-        languages = simplejson.loads(content.read());
-        memcache.add('languages', languages, 604800) # 1 week
-    
-    values = {'languages': languages}
+    values = {'languages': LANGUAGES}
     path = os.path.join(os.path.dirname(__file__), 'index.html')
     self.response.out.write(template.render(path, values))
 
@@ -49,11 +96,9 @@ class TranslateHandler(webapp.RequestHandler):
     url1 = 'http://ajax.googleapis.com/ajax/services/language/translate?' + urlencode(params1)
     
     params2 = {
-      'unknown_text': q.encode('utf-8'),
-      'text': None,
-      'mode': 1
+      'type': 'romaji'
     }
-    url2 = 'http://romaji.udp.jp/romaji.cgi'
+    url2 = 'http://tatoeba.org/eng/tools/kakasi?'
     
     if q:
       if not lang: lang = 'auto' # make sure 'q' won't have same value as 'lang'
@@ -72,18 +117,13 @@ class TranslateHandler(webapp.RequestHandler):
               response['japanese'] = content['responseData']['translatedText']
               
               try:
-                params2['text'] = response['japanese'].encode('utf-8')
-                result2 = urlfetch.fetch(url2, payload=urlencode(params2), method=urlfetch.POST, headers={'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}, deadline=10)
+                params2['query'] = response['japanese'].encode('utf-8')
+                result2 = urlfetch.fetch(url2 + urlencode(params2), allow_truncated=True, deadline=10)
                 if result2.status_code == 200:
-                  content = parseString(result2.content)
-                  text = []
-                  rts = content.getElementsByTagName('rt')
-                  for rt in rts:
-                    nodelist = rt.childNodes
-                    for node in nodelist:
-                      if node.nodeType == node.TEXT_NODE and node.data:
-                        text += [node.data]
-                  response['romaji'] = ' '.join(text)
+                  stainer = SoupStrainer(id='conversion')
+                  soup = BeautifulSoup(result2.content, parseOnlyThese=stainer)
+                  text = soup.contents[0].string.replace('\n', '').strip()
+                  response['romaji'] = text
                   
                   json = simplejson.dumps(response, indent=4, ensure_ascii=False)
                   memcache.add(lang + q, json, 86400) # 1 day
